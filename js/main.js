6a37d4772fb9b3b4f709a0651a3d955a54acd2cc
Magomedow <92452647+Magomedow@users.noreply.github.com>
2022-04-27 02:51:14 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,59 @@
+$(function(){
+   $(".header-slider").slick({
+      dots:true,
+      prevArrow:'<button type="button" class="slick-prev"><img src="images/strel.svg" alt=""></button>',
+      nextArrow:'<button type="button" class="slick-next"><img src="images/emd.svg" alt=""></button>',
+      fade: true,
+      responsive:[
+         {
+            breakpoint:371,
+            settings:  {
+               dots: false,
+               arrows:false,
+               autoplay: true,
+               autoplaySpeed: 2000,
+         }
+      }
+      ]
+   });
+
+   $('.product__name').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      focusOnSelect: true,
+      asNavFor: '.product__content',
+      vertical:true,
+      prevArrow:'<button type="button" class="product-prev"><img src="images/bbepx.svg" alt=""></button>',
+      nextArrow:'<button type="button" class="product-next"><img src="images/Vniz.svg" alt=""></button>',
+      responsive:[
+         {
+            breakpoint:891,
+            settings:  {
+            vertical:false,
+            slidesToShow: 3,
+            arrows:false,
+            dots:true,
+         },
+         breakpoint:461,
+         settings:  {
+         slidesToShow: 1,
+         vertical:false,
+         arrows:false,
+         dots:true,}
+      }
+      ]
+    });
+    $('.product__content').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      asNavFor: '.product__name',
+      focusOnSelect: true,
+      fade:true,
+      arrows:false
+    });
+
+    $('.menu__btn').on('click', function(){
+       $('.menu__list').toggleClass('menu__list--active');
+    });
+
+});
